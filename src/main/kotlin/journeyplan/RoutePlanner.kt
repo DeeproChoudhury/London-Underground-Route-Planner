a56095e4c157c7661b9227375dcb870c80e2acd4
DeeproChoudhury <72229274+DeeproChoudhury@users.noreply.github.com>
2020-11-27 11:36:57 +0000

    All normal tests working and added extension to optimise route

--- src/main/kotlin/journeyplan/RoutePlanner.kt
+++ src/main/kotlin/journeyplan/RoutePlanner.kt
@@ -13,25 +13,27 @@ class Line (private val lineName: String) {
     }
 }
 
-class Segment (val station1:Station, val station2:Station, linkedLine:Line, time:Int) {
+class Segment (val station1:Station, val station2:Station, val line:Line, val time:Int) {
 
 }
 class SubwayMap (val map:List<Segment>) {
-    fun routesFrom(origin: Station, destination: Station): List<Route> {
-        fun helper(currentSegment: Segment, visitedStations: List<Segment>): List<Segment> {
-            val segments = map.filter { x -> x.station1 == currentSegment.station2 && x.station2 !in visitedStations }
-            visitedStations.plus(currentSegment.station2)
-            for (segment in segments) {
-                return if (segment.station1 == destination) {
-                    emptyList()
-                } else {
-                    listOf(currentSegment).plus(helper(segment, visitedStations))
-                }
-            } ; return listOf(currentSegment)
+    fun routesFrom(origin: Station, destination: Station, optimisingFor: (Route) -> Int = Route :: duration): List<Route> {
+        val listRoutes = helper(origin, destination, emptySet())
+        return listRoutes.sortedBy(optimisingFor)
+    }
+    private fun helper(origin: Station, destination: Station, visitedStations: Set<Station>): List<Route> {
+        if (origin == destination) {
+            return listOf(Route(emptyList()))
         }
+        val segments = map.filter { x -> x.station1 == origin && x.station2 !in visitedStations }
+
+        //return segments.flatMap { segment -> listOf(Route(listOf(segment))).plus(helper(segment.station2, destination, visitedStations.plus(origin)))}
+        return segments.flatMap { segment -> helper(segment.station2, destination, visitedStations.plus(origin)).map {Route (listOf(segment) + it.segments)}}
     }
 
 }
+
+
 
 fun londonUnderground(): SubwayMap {
     val actonT = Station("Acton Town")
@@ -76,35 +78,41 @@ fun londonUnderground(): SubwayMap {
             Segment(sloaneSquare, victoria, districtLine, 6)
         )
     )
+    println(subwaymap.routesFrom(victoria, greenPark).first())
     return subwaymap
 
 }
 
 class Route(val segments: List<Segment>) {
     override fun toString(): String {
-        val overview = "${segments.first().station1} to ${segments.last().station2}\n"
-        return overview + segments.joinToString(prefix = "", postfix = "", separator = "\n")
+        val sb = StringBuilder ()
+        sb.append("${segments.first().station1} to ${segments.last().station2} - ${duration()} minutes, ${numChanges()} changes\n")
+        var currentLine = segments.first().line
+        sb.append(" - ${segments.first().station1} to ")
+        for (segment in segments) {
+            if (segment.line != currentLine) {
+                sb.append("${segment.station1} by $currentLine\n")
+                sb.append(" - ${segment.station1} to ")
+                currentLine = segment.line
+            }
+        }
+        sb.append("${segments.last().station2} by $currentLine")
+        return sb.toString()
     }
 
+    fun duration () : Int {
+        return segments.map {it.time}.sum()
+    }
+
+    fun numChanges (): Int {
+        return segments.zipWithNext().filter { (x, y) -> x.line != y.line }.size
+    }
 }
 
 fun main () {
 
-    val northernLine = Line("Northern")
-    val victoriaLine = Line("Victoria")
-    val centralLine = Line("Central")
 
-    val highgate = Station("Highgate")
-    val archway = Station("Archway")
-    val tufnellPark = Station("Tufnell Park")
-    val kentishTown = Station("Kentish Town")
-    val camden = Station("Camden Town")
-    val euston = Station("Euston")
-    val warrenStreet = Station("Warren Street")
-    val oxfordCircus = Station("Oxford Circus")
-    val bondStreet = Station("Bond Street")
-
-    var subwaymap = londonUnderground()
+    val map = londonUnderground()
 
 
 
